009d8f5ab1f3c7e50fffe3b25ed02d0ede2a6455
NguyenLamTruong-DEVWEB <nguyenlamtruong.devweb@gmail.com>
2022-06-27 11:14:25 +0700

    connect data

--- system/database.php
+++ system/database.php
@@ -3,15 +3,40 @@ include 'config.php';
 include '../app/Ab_database/Ab_database.php';
 // class kết nối với database
 class Database extends  Connection_data {
-    public $servename;
-    public $username;
-    public $password;
-    public $dbname;
+    public $servename = servername;
+    public $username = username;
+    public $password = password;
+    public $dbname = dbname;
+
+    public $conn;    //  biến conn nhận kết nối v
+
     // phương thức kết nối
     public function ConnectData(){
-        echo 'hello';
+        $opt = array(
+            PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+        );
+        $this->conn = new PDO(
+            'mysql:host='.$this->servename.';dbname='.$this->dbname,
+            $this->username, $this->password, $opt
+        );
     }
-
+    // query database 
+    public function query($sql){
+        $result = $this->conn->query($sql);
+        return $result;
+    }
+    // query 1 dòng dữ liệu database
+    public function queryOne($sql){
+        $data = $this->conn->query($sql);
+        $result = $data->fetch();
+        return $result;
+    }
+    // thêm , xóa , sửa dữ liệu database
+    public function excute($sql){
+        $result = $this->conn->exec($sql);
+        return $result;
+    }
     public function DesConn(){
         echo $this->servename;
     }
